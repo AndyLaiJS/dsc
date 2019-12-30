b5a324519e4eb82352909c6460f5428d87d6203b
AndyPhoenix9879 <Lai9879m@gmail.com>
2019-12-30 21:54:28 +0800

    Temporary Intro section text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,19 @@
 <template>
-  <div class="home">
-
+  <div class="IntroSection">
+    <div class="Content">
+      <h1> Developer Student Club @ CUHK </h1>
+      <p> 
+        Developer Student Club @ CUHK
+        Developer Student Clubs is a  Google 
+        Developers program for university 
+        students to learn  mobile  and  web 
+        development skills, design thinking skills  
+        and  leadership skills.
+      </p>
+      </div>
+    <div class="doodle">
+      DOODLE
+    </div>
   </div>
 </template>
 
@@ -10,6 +23,18 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.IntroSection {
+  margin: 10% 0 10% 0;
+  display: flex;
+  flex-wrap: wrap;
+}
+.Content {
+  text-align: left;
+  width: 30%;
+  margin-left: 15%;
+  p {
+    font-size: 20px;
+  }
+}
 </style>
