312c60a9ad97a3bc1d24c5d6e2161781dfb8390d
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-01-04 17:32:20 +0800

    gdsc build alpha-0

---
+++ docs/precache-manifest.76ebc4785f0f664031ba67b827d5bc55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0291f30ac3d3ef3ee04e",
+    "url": "dsc/css/app.f1378af0.css"
+  },
+  {
+    "revision": "244b6bdd4dbfede62d769c2c806ab040",
+    "url": "dsc/img/audience.244b6bdd.png"
+  },
+  {
+    "revision": "611b79855f8c54782c10a3f12ce0ed8c",
+    "url": "dsc/img/handshake.611b7985.png"
+  },
+  {
+    "revision": "35f4aa98635d5850ce7b1b1cf3240996",
+    "url": "dsc/img/lightbulb.35f4aa98.png"
+  },
+  {
+    "revision": "c3b28d6f9d3ba32542d34b8b78c192a0",
+    "url": "dsc/index.html"
+  },
+  {
+    "revision": "0291f30ac3d3ef3ee04e",
+    "url": "dsc/js/app.457084db.js"
+  },
+  {
+    "revision": "c704909d83c4ccd5070e",
+    "url": "dsc/js/chunk-vendors.f7978236.js"
+  },
+  {
+    "revision": "709cbf1dbfe0efcbb2a83a7abe388f9a",
+    "url": "dsc/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "dsc/robots.txt"
+  }
+]);
