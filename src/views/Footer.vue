a244454ac1a89b7fe87579b9ce00561785ed8a55
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-01-07 14:56:52 +0800

    Footer

---
+++ src/views/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="footerBody">
+        <h2>DSC CUHK</h2>
+        <p>Copyright © 2019 | All rights reserved.</p>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* temporary h2 */
+h2 {
+    font-weight: 100;
+}
+.footerBody {
+    text-align: left;
+    background-color: white;
+    padding: 100px 15%;
+}
+</style>
