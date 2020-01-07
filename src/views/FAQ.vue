7a83a61e912586076fa632cfa90bad6f91ea93dc
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-01-07 15:41:01 +0800

    FAQ

---
+++ src/views/FAQ.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="faqBody">
+        <h1>Frequently Asked Questions</h1>
+
+        <div class="bodyContent">
+            <span style="width: 500px;">
+                a
+            </span>
+
+            <span style="width: 500px;">
+                b
+            </span>
+
+            <span style="width: 500px;">
+                c
+            </span>
+
+            <span style="width: 500px;">
+                d 
+            </span>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+.faqBody {
+    background-color: white;
+    padding: 2% 0 3% 0;
+}
+.bodyContent {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+}
+</style>
