0c8ace1454182a79513691ad743a90ae99a14d55
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-01-07 22:19:37 +0800

    Footer: Restructuring

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,7 +1,26 @@
 <template>
     <div class="footerBody">
-        <h2>DSC CUHK</h2>
-        <p>Copyright © 2019 | All rights reserved.</p>
+        <span id="left">
+            <h2>DSC CUHK</h2>
+            Powered by 
+            <b>
+            <span id="gblue">G</span>
+            <span id="gred">o</span>
+            <span id="gyel">o</span>
+            <span id="gblue">g</span>
+            <span id="ggre">l</span>
+            <span id="gred">e</span> 
+            </b> Developers
+            <p>Copyright © 2020 | All rights reserved.</p>
+            <div class="socialIcon">
+                <img style="height: 21px" src="../assets/f_logo.png">
+                <img style="height: 20px" src="../assets/ig.png">
+            </div>
+        </span>
+
+        <span id="right">
+            igjijgisajfgijaginaijgasidj
+        </span>
     </div>
 </template>
 
@@ -15,10 +34,21 @@ export default {
 /* temporary h2 */
 h2 {
     font-weight: 100;
+    margin: 0;
 }
 .footerBody {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
     text-align: left;
     background-color: white;
-    padding: 100px 15%;
+    padding: 75px 15%;
+}
+.socialIcon {
+    display: grid;
+    grid-template-columns: 30px 30px;
+}
+img {
+    filter: invert(100%);
 }
 </style>
