49cce4b8210a3c81d5e96c9e06b383c055deb35a
AndyPhoenix9879 <Lai9879m@gmail.com>
2020-01-07 22:19:15 +0800

    FAQ: Completed contents

--- src/views/FAQ.vue
+++ src/views/FAQ.vue
@@ -3,20 +3,31 @@
         <h1>Frequently Asked Questions</h1>
 
         <div class="bodyContent">
-            <span style="width: 500px;">
-                a
+            <span>
+                <h3>Do I need to be a CS student to join?</h3>
+                <p>No, anyone and everyone is welcomed to join the event, regardless of your major and background. After all, we are all here
+                    to learn something new!
+                </p>
             </span>
 
-            <span style="width: 500px;">
-                b
+            <span>
+                <h3>What do I have to bring to the event?</h3>
+                <p>We recommend you bring a notebook, pen, and a laptop when attending these events, but the most important of all is
+                    to carry a strong dose of curiosity and enthusiasm!
+                </p>
             </span>
 
-            <span style="width: 500px;">
-                c
+            <span>
+                <h3>Who should I reach out to if I have any questions?</h3>
+                <p>If you have any questions or comments, please don't hesitate to contact us by <a target="_blank" href="mailto:dsc.cuhk@gmail.com">email</a>. 
+                    We would be more than happy to stay engaged via email even after the event
+                </p>
             </span>
 
-            <span style="width: 500px;">
-                d 
+            <span>
+                <h3>Do I need to pay a membership fee to join?</h3>
+                <p>Nope! Every thing organized by DSC CUHK - from events, workshops, talks and all, are all free for anyone to attend and join.
+                </p>
             </span>
 
         </div>
@@ -29,14 +40,19 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .faqBody {
     background-color: white;
-    padding: 2% 0 3% 0;
+    padding: 2% 15% 3% 15%;
 }
 .bodyContent {
+    justify-content: space-evenly;
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
+    span {
+        width: 450px;
+        text-align: left;
+    }
 }
 </style>
